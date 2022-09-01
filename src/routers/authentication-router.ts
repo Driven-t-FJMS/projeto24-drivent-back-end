9ed881bd4b996305974cb5b94d9640f4a8c41560
Samuel Marco <samuelmarco.2020@alunos.utfpr.edu.br>
2022-09-01 01:22:05 -0300

    feat: implementation to create user and session according to a code provided by access authorization on GitHub via OAuth protocol

--- src/routers/authentication-router.ts
+++ src/routers/authentication-router.ts
@@ -1,10 +1,11 @@
-import { singInPost } from '@/controllers';
+import { singInPost, singInPostOAuth } from '@/controllers';
 import { validateBody } from '@/middlewares';
-import { signInSchema } from '@/schemas';
+import { signInSchema, codeAuthGitHub } from '@/schemas';
 import { Router } from 'express';
 
 const authenticationRouter = Router();
 
 authenticationRouter.post('/sign-in', validateBody(signInSchema), singInPost);
+authenticationRouter.post('/sign-in/github', validateBody(codeAuthGitHub), singInPostOAuth);
 
 export { authenticationRouter };
--- src/services/users-service/index.ts
+++ src/services/users-service/index.ts
@@ -35,6 +35,8 @@ export type CreateUserParams = Pick<User, 'email' | 'password'>;
 
 const userService = {
   createUser,
+  canEnrollOrFail,
+  validateUniqueEmailOrFail,
 };
 
 export * from './errors';
